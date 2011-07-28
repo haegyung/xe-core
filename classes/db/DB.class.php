166ae08c8d00ea307af7f7c7f30881ed2e3099b6
namnyams@gmail.com <namnyams@gmail.com@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2011-07-28 05:11:37 +0000

    issue 95 modify query number filter
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.4.5@8680 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/db/DB.class.php
+++ classes/db/DB.class.php
@@ -701,6 +701,7 @@
 		function _filterNumber(&$value)
 		{
 			$value = preg_replace('/[^\d\w\+\-\*\/\.\(\)]/', '', $value);
+			$value = preg_replace('@[/+\-*]{2,}@', '', $value);
 			if(!$value) $value = 0;
 		}
     }
