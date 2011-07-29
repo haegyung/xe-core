2ba674058a8f70486085f55cac2811e304684c9b
namnyams@gmail.com <namnyams@gmail.com@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2011-07-29 07:35:04 +0000

    issue 97 admin action grant check
    git-svn-id: http://xe-core.googlecode.com/svn/branches/1.4.5@8699 201d5d3c-b55e-5fd7-737f-ddc643e51545

--- classes/module/ModuleHandler.class.php
+++ classes/module/ModuleHandler.class.php
@@ -285,6 +285,17 @@
                             return $oMessageObject;
                         }   
 					}
+					if ($kind == 'admin'){
+						if($logged_info->is_admin!='Y') {
+                            $this->error = 'msg_is_not_administrator';
+                            $oMessageObject = &ModuleHandler::getModuleInstance('message',$type);
+                            $oMessageObject->setError(-1);
+                            $oMessageObject->setMessage($this->error);
+                            $oMessageObject->dispMessage();
+                            return $oMessageObject;
+                        }   
+						
+					}
 				}
 				else if($xml_info->default_index_act && method_exists($oModule, $xml_info->default_index_act))
 				{
